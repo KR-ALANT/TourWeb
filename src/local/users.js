a722d31dd45d4715844142f23699d4fb4ab7ae49
kimryungu <ryunguworks@gmail.com>
2024-07-22 15:15:20 +0900

    local

---
+++ src/local/users.js
@@ -0,0 +1,64 @@
+export const Users = [
+    {
+        "id": 1,
+        "first_name": "강릉",
+        
+    },
+    {
+        "id": 2,
+        "first_name": "원주",
+        
+    },
+    {
+        "id": 3,
+        "first_name": "춘천",
+        
+    },
+    {
+        "id": 4,
+        "first_name": "부산",
+        
+    },
+    {
+        "id": 5,
+        "first_name": "대전",
+        
+    },
+    {
+        "id": 6,
+        "first_name": "수원",
+        
+    },
+    {
+        "id": 7,
+        "first_name": "제주도",
+        },
+    {
+        "id": 8,
+        "first_name": "경주",
+        },
+    {
+        "id": 9,
+        "first_name": "Florida",
+    },
+    {
+        "id": 10,
+        "first_name": "Gerianne",
+    },
+    {
+        "id": 11,
+        "first_name": "Amy",
+    },
+    {
+        "id": 12,
+        "first_name": "Jerrilyn", 
+    },
+    {
+        "id": 13,
+        "first_name": "Derrik",
+        },
+    {
+        "id": 14,
+        "first_name": "Kellia",
+    }
+]
