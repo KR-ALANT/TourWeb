74ad07c3fda2d3f3e02875d19a06a0af8290d3c3
kimryungu <ryunguworks@gmail.com>
2024-08-06 21:27:20 +0900

    Appjs

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import MainPage from './main/MainPage';
+import Local from './local//Local';
+import Calendar from './calendar/Calendar';
+import Map from './map/Map';
+import Community from './community/Community';
+
+function App() {
+  return (
+    <Routes>
+      <Route path = "/" element = {<MainPage />} />
+      <Route path = "/local/Local" element = {<Local />} />
+      <Route path = "/calendar/Calendar" element = {<Calendar />} />
+      <Route path = "/map/Map" element = {<Map />} />
+      <Route path = "/community/Community" element = {<Community />} />
+    </Routes>
+  )
+}
+
+export default App;
